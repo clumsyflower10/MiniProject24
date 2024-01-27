337d0a2e63fed4d10112da3e8fe18445b9ebeee6
clumsyflower10 <yuniar.flo@gmail.com>
2024-01-27 13:30:15 -0500

    navbar button

--- apps/web/src/Pages/login.tsx
+++ apps/web/src/Pages/login.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import Login from '../components/Login';
+import { Footer } from '../components/Footer';
+import Header from '../components/Header';
 import '../app/globals.css';
 
 const loginPage = () => {
   return (
     <div>
-      <Login />
+      <div>
+        <div>
+          <Header />
+        </div>
+        <Login />
+      </div>
+      <div>
+        <Footer />
+      </div>
     </div>
   );
 };
